2bb03f4ad631c88a4673555cfc6247c90cff8109
jljulioa <jljulioa@gmail.com>
2024-03-31 16:29:40 -0500

    CI/CD_Docker_ECR

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:21.7.1-alpine
+
+RUN apk tzdata update && apk add --no-cache tzdata
+
+ENV TZ=America/Bogota
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 8080
+
+CMD ["npm", "run", "dev"]
--- src/libs/wall.upload.js
+++ src/libs/wall.upload.js
@@ -20,8 +20,6 @@ export const uploadS3 = async (filename, file, type) => {
 
   console.log(objectKey);
 
-  // Set the expiration time for the pre-signed URL (in seconds)
-  //const expirationSeconds = 120; 
 
   const command = new PutObjectCommand({
     Body: file.buffer,
@@ -33,11 +31,6 @@ export const uploadS3 = async (filename, file, type) => {
   try {
 
     await s3Client.send(command);
-    /* const url = await getSignedUrl(s3Client, putObjectCommand, {
-      expiresIn: expirationSeconds,
-    });
-
-    return url */
 
   } catch (error) {
     console.error("Error generating pre-signed URL for upload:", error);
