cbf94e641f0c1a49bb8e304c226d442ebcdd7377
jljulioa <jljulioa@gmail.com>
2024-04-10 15:19:40 -0500

    test

--- src/app.js
+++ src/app.js
@@ -5,11 +5,16 @@ import cookieparser from 'cookie-parser'
 import taskRouter from './routes/task.route.js'
 import cors from 'cors'
 
+const allowedOrigins = [
+    '54.211.21.161',
+    '54.243.21.80',
+  ];
+
 
 const app = express()
 
 app.use(cors({
-    origin: '[54.211.21.161, 54.243.21.80]',
+    origin: allowedOrigins,
     credentials: true
 }))
 app.use(morgan('dev'));
